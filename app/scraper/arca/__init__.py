a4b254b32d97e3323e1633e1630c41f28b6849c3
Snuff <snuff8729@gmail.com>
2026-04-18 18:08:45 +0900

    feat: export public API from arca scraper package

---
+++ app/scraper/arca/__init__.py
@@ -0,0 +1,21 @@
+from app.scraper.arca.channel import ArcaChannel
+from app.scraper.arca.client import ArcaClient
+from app.scraper.arca.models import (
+    ArticleDetail,
+    ArticleList,
+    ArticleRow,
+    Attachment,
+    Category,
+    Comment,
+)
+
+__all__ = [
+    "ArcaClient",
+    "ArcaChannel",
+    "ArticleDetail",
+    "ArticleList",
+    "ArticleRow",
+    "Attachment",
+    "Category",
+    "Comment",
+]
